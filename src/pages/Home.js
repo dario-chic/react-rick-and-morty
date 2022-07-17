dd7d71cd7ba02fc3f81d99c7d9c2e15081ae1380
Dario Chic <dariochic@gmail.com>
2022-07-16 22:49:26 -0400

    Commit del Home para resolver lo de las imagenes que no se encuentran

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { useContext } from "react";
+import Resume from "../components/Resume";
+import ThemeContext from "../context/ThemeContext";
 
 const Home = () => {
+  const { theme } = useContext(ThemeContext);
+
   return (
-    <div>
-      <h2>Home</h2>
+    <div className={`home ${theme}`}>
+      <Resume theme={theme} />
     </div>
   );
 };
